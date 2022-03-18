ed47e9d013d4adebf584a6eb8101be8a5795abcb
raear <raear@nih.gov>
2022-03-18 16:15:38 -0400

    Add package long description

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='mtix_descriptor_prediction_pipeline',
       version='0.1',
       description='MTIX MeSH Descriptor prediction pipeline.',
+      long_description=readme(),
       url='https://github.com/ncbi/mtix/',
       author='Alastair Rae',
       author_email='',
       license='',
       packages=['mtix_descriptor_prediction_pipeline'],
+      install_requires=[],
+      include_package_data=True,
       zip_safe=False)
