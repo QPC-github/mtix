cc979d4a5fa7a0fe99eaea96fd15ae5cf742ef0f
raear <raear@nih.gov>
2022-03-24 13:39:13 -0400

    Move test data into package

--- src/mtix_descriptor_prediction_pipeline/tests/test_pipeline.py
+++ src/mtix_descriptor_prediction_pipeline/tests/test_pipeline.py
@@ -1,12 +1,15 @@
+import gzip
 import json
 from mtix_descriptor_prediction_pipeline.pipeline import DescriptorPredictionPipeline, MedlineDateParser, PubMedXmlInputDataParser, MtiJsonResultsFormatter
 from mtix_descriptor_prediction_pipeline.predictors import CnnModelTopNPredictor, PointwiseModelTopNPredictor, ListwiseModelTopNPredictor
+import os.path
 from unittest import TestCase
 from unittest.mock import MagicMock
 
-# TODO: Data should be included in package
-TEST_SET_DATA_PATH = "/home/raear/working_dir/mtix/scripts/create_test_set_data/test_set_data.json"
-TEST_SET_PREDICTIONS_PATH = "/home/raear/working_dir/mtix/scripts/create_test_set_predictions/test_set_2017-2022_Listwise22Avg_Results.json"
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+TEST_SET_DATA_PATH = os.path.join(THIS_DIR, "data", "test_set_data.json.gz")
+TEST_SET_PREDICTIONS_PATH = os.path.join(THIS_DIR, "data", "test_set_2017-2022_Listwise22Avg_Results.json.gz")
 
 
 UNIT_TEST_DATA = [
@@ -65,8 +68,8 @@ class TestDescriptorPredictionPipeline(TestCase):
         listwise_predictor = ListwiseModelTopNPredictor({}, 50)
         results_formatter = MtiJsonResultsFormatter({}, 0.475)
         pipeline = DescriptorPredictionPipeline(input_data_parser, cnn_predictor, pointwise_predictor, listwise_predictor, results_formatter)
-        input_data = json.load(open(TEST_SET_DATA_PATH))
-        expected_predictions = json.load(open(TEST_SET_PREDICTIONS_PATH))
+        input_data = json.load(gzip.open(TEST_SET_DATA_PATH))
+        expected_predictions = json.load(gzip.open(TEST_SET_PREDICTIONS_PATH))
         predictions = pipeline.predict(input_data)
         self.assertEqual(predictions, expected_predictions, "Predictions do not match expected result.")
 
