f8b00e57af7af3210d6ff9486d399fbeb170a183
raear <raear@nih.gov>
2022-04-05 16:18:22 -0400

    List requirements in setup.py

--- setup.py
+++ setup.py
@@ -13,8 +13,16 @@ setup(name='mtix_descriptor_prediction_pipeline',
       author_email='',
       license='',
       packages=['mtix_descriptor_prediction_pipeline'],
-      install_requires=[],
+      python_requires='3.10.2'
+      install_requires=[
+          "pandas==1.4.1",
+          "python-dateutil==2.8.2",
+          "pytrec-eval==0.5",
+          "sagemaker-python-sdk==2.80.0",
+          "zlib==1.2.11"
+      ],
       include_package_data=True,
       zip_safe=False
       test_suite='nose.collector',
-      tests_require=['nose'])
+      tests_require=['nose']
+      )
