5c465a41111ebae3c818c3d76bed52527f81f0d1
raear <raear@nih.gov>
2022-04-06 16:42:40 -0400

    Specify model versions

--- scripts/create_sagemaker_endpoints.py
+++ scripts/create_sagemaker_endpoints.py
@@ -4,11 +4,14 @@ from sagemaker.tensorflow import TensorFlowModel
 
 INSTANCE_TYPE = "ml.m5.xlarge"
 S3_BUCKET = "s3://raear-hf-sagemaker-inference"
+CNN_MODEL_FILENAME = "cnn_model_2022_v1.tar.gz"
+POINTWISE_MODEL_FILENAME = "pointwise_model_2022_v1.tar.gz"
+LISTWISE_MODEL_FILENAME = "listwise_model_2022_v1.tar.gz"
 
 
 def create_cnn_model_endpoint():
     model = TensorFlowModel(
-        model_data=f"{S3_BUCKET}/cnn_model.tar.gz",
+        model_data=f"{S3_BUCKET}/{CNN_MODEL_FILENAME}",
         role="SageMakerExecute",
         framework_version="2.8.0",
     )
@@ -22,7 +25,7 @@ def create_cnn_model_endpoint():
 
 def create_pointwise_model_endpoint():
     huggingface_model = HuggingFaceModel(
-        model_data=f"{S3_BUCKET}/pointwise_model.tar.gz",
+        model_data=f"{S3_BUCKET}/{POINTWISE_MODEL_FILENAME}",
         role="SageMakerExecute",
         transformers_version="4.12.3",
         pytorch_version="1.9.1",
@@ -39,7 +42,7 @@ def create_pointwise_model_endpoint():
 
 def create_listwise_model_endpoint():
     huggingface_model = HuggingFaceModel(
-        model_data=f"{S3_BUCKET}/listwise_model.tar.gz",
+        model_data=f"{S3_BUCKET}/{LISTWISE_MODEL_FILENAME}",
         role="SageMakerExecute",
         transformers_version="4.12.3",
         pytorch_version="1.9.1",
@@ -52,7 +55,6 @@ def create_listwise_model_endpoint():
     )
     print("Listwise model endpoint name:")
     print(predictor.endpoint_name)
-    predictor.delete_endpoint()
 
 
 def main():
