37d9097a61deac82bffb005567c1ccf28a16c45e
raear <raear@nih.gov>
2023-01-19 18:12:58 -0500

    Update model/endpoint names for 2023 MeSH

--- scripts/create_sagemaker_endpoints.py
+++ scripts/create_sagemaker_endpoints.py
@@ -5,10 +5,10 @@ from sagemaker.tensorflow import TensorFlowModel
 
 INSTANCE_TYPE = "ml.g4dn.2xlarge"
 S3_BUCKET = "s3://ncbi-aws-pmdm-ingest"
-CNN_MODEL_FILENAME = "cnn_model_2022_v1.tar.gz"
-POINTWISE_MODEL_FILENAME = "pointwise_model_2022_v2.tar.gz"
-LISTWISE_MODEL_FILENAME = "listwise_model_2022_v2.tar.gz"
-SUBHEADING_MODEL_FILENAME = "all_subheading_cnn_model_2022_v1.tar.gz"
+CNN_MODEL_FILENAME = "cnn_model_2023_v1.tar.gz"
+POINTWISE_MODEL_FILENAME = "pointwise_model_2023_v1.tar.gz"
+LISTWISE_MODEL_FILENAME = "listwise_model_2023_v1.tar.gz"
+SUBHEADING_MODEL_FILENAME = "all_subheading_cnn_model_2023_v1.tar.gz"
 
 
 def create_cnn_model_endpoint():
@@ -16,14 +16,14 @@ def create_cnn_model_endpoint():
         model_data=f"{S3_BUCKET}/mtix-models/{CNN_MODEL_FILENAME}",
         role="AmazonSageMaker-ExecutionRole-PMDM",
         framework_version="2.8.0",
-        name="raear-cnn-model-2022-v1-async"
+        name="raear-cnn-model-2023-v1-async"
     )
     output_path = f"{S3_BUCKET}/async_inference/cnn_endpoint/outputs/"
     async_config = AsyncInferenceConfig(output_path=output_path)
     model.deploy(
         initial_instance_count=1,
         instance_type=INSTANCE_TYPE,
-        endpoint_name="raear-cnn-endpoint-2022-v1-async",
+        endpoint_name="raear-cnn-endpoint-2023-v1-async",
         async_inference_config=async_config,
     )
 
@@ -36,14 +36,14 @@ def create_pointwise_model_endpoint():
         pytorch_version="1.9.1",
         py_version="py38",
         env={ "HF_TASK": "text-classification" },
-        name="raear-pointwise-model-2022-v2-async"
+        name="raear-pointwise-model-2023-v1-async"
     )
     output_path = f"{S3_BUCKET}/async_inference/pointwise_endpoint/outputs/"
     async_config = AsyncInferenceConfig(output_path=output_path)
     huggingface_model.deploy(
         initial_instance_count=1,
         instance_type=INSTANCE_TYPE,
-        endpoint_name="raear-pointwise-endpoint-2022-v2-async",
+        endpoint_name="raear-pointwise-endpoint-2023-v1-async",
         async_inference_config=async_config
     )
 
@@ -55,14 +55,14 @@ def create_listwise_model_endpoint():
         pytorch_version="1.9.1",
         py_version="py38",
         env={ "HF_TASK": "token-classification" },
-        name="raear-listwise-model-2022-v2-async"
+        name="raear-listwise-model-2023-v1-async"
     )
     output_path = f"{S3_BUCKET}/async_inference/listwise_endpoint/outputs/"
     async_config = AsyncInferenceConfig(output_path=output_path)
     huggingface_model.deploy(
         initial_instance_count=1,
         instance_type=INSTANCE_TYPE,
-        endpoint_name="raear-listwise-endpoint-2022-v2-async",
+        endpoint_name="raear-listwise-endpoint-2023-v1-async",
         async_inference_config=async_config
     )
 
@@ -71,14 +71,14 @@ def create_subheading_endpoint():
         model_data=f"{S3_BUCKET}/mtix-models/{SUBHEADING_MODEL_FILENAME}",
         role="AmazonSageMaker-ExecutionRole-PMDM",
         framework_version="2.8.0",
-        name="raear-all-subheading-cnn-model-2022-v1-async"
+        name="raear-all-subheading-cnn-model-2023-v1-async"
     )
     output_path = f"{S3_BUCKET}/async_inference/subheading_endpoint/outputs/"
     async_config = AsyncInferenceConfig(output_path=output_path)
     model.deploy(
         initial_instance_count=1,
         instance_type=INSTANCE_TYPE,
-        endpoint_name="raear-all-subheading-cnn-endpoint-2022-v1-async",
+        endpoint_name="raear-all-subheading-cnn-endpoint-2023-v1-async",
         async_inference_config=async_config
     )
 
